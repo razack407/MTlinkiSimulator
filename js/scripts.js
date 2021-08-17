c302b942f65fa7bafdd89d7d5401802984738959
razack407 <razack407@gmail.com>
2021-08-17 11:42:15 +0530

    relative path 56

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,9 @@
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+document.addEventListener ('DOMContentLoaded',function() {
+    "use strict";
+    console.log ('now we are ready');
+})
+var myid=document.getElementById('inputShift1From')
+console.log (myid.value);
